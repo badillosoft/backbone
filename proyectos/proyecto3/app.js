90c8739a8822de6f4f140557dd7fec5419c5edc1
Dragon <dragon@hsoft.local>
2019-04-08 13:42:54 -0500

    sesión 3

--- proyectos/proyecto3/app.js
+++ proyectos/proyecto3/app.js
@@ -1,7 +1,10 @@
+const Productos = require("./colecciones/Productos");
 const Producto = require("./modelos/Producto");
 const TarjetaProducto = require("./vistas/TarjetaProducto");
 
-const producto = new Producto({
+const productos = new Productos();
+
+productos.add(new Producto({
     id: "coca-cola-600",
     nombre: "Coca-Cola 600ml",
     descripcion: "Refresco Coca-Cola de 600ml",
@@ -9,16 +12,23 @@ const producto = new Producto({
     costo: 6.0,
     existencias: 728,
     etiquetas: ["refresco", "coca-cola", "600ml"]
-});
+}));
+productos.add(new Producto({
+    id: "marias-10",
+    nombre: "Galletas Marías 10pz",
+    descripcion: "Galletas Marías de 10pz empaque dorado",
+    precio: 8.5,
+    costo: 7.0,
+    existencias: 32,
+    etiquetas: ["galleta", "gamesa"]
+}));
+productos.add(new Producto({
+    id: "pepsi-600",
+    nombre: "Pepsi 600ml",
+    descripcion: "Refresco Pepsi de 600ml",
+    precio: 11.5,
+    costo: 6.5,
+    existencias: 43,
+    etiquetas: ["refresco", "pepsi", "600ml"]
+}));
 
-console.log(producto);
-
-const tarjetaProducto = new TarjetaProducto({
-    model: producto
-});
-
-document.body.appendChild(tarjetaProducto.el);
-
-setTimeout(() => {
-    producto.set("existencias", 2);
-}, 5000);
--- proyectos/proyecto3/colecciones/Productos.js
+++ proyectos/proyecto3/colecciones/Productos.js
@@ -1,1 +1,3 @@
-module.exports = 
+module.exports = Backbone.Collection.extend({
+    
+});
