8a39efb7299227f27e763b6eac6529f27bbf9ee8
Dragon <dragon@hsoft.local>
2019-04-09 12:51:42 -0500

    sesión 4

--- proyectos/proyecto3/vistas/EstadisticosProductos.js
+++ proyectos/proyecto3/vistas/EstadisticosProductos.js
@@ -1,11 +1,15 @@
 module.exports = Backbone.View.extend({
     template: _.template(`
-        <p>Total de productos: 1000</p>
+        <style>
+            li {
+                margin-left: 20px;
+            }
+        </style>
+        <p>Total de productos: <%= total %></p>
         <ul>Etiquetas de productos:
-            <li>REFRESCO: 100</li>
-            <li>PAN: 23</li>
-            <li>GALLETA: 55</li>
-            <li>GAMESA: 19</li>
+            <% for (let nombre in etiquetas) { %>
+                <li><%= nombre.toUpperCase() %>: <%= etiquetas[nombre] %></li>
+            <% } %>
         </ul>
     `),
     initialize() {
@@ -13,7 +17,25 @@ module.exports = Backbone.View.extend({
         this.render();
     },
     render() {
-        this.$el.html(this.template());
-        console.log("estadísticos");
+        // this.collection.reduce(callback, reductor);
+        // reductor -> {}
+        // callback(reductor, item)
+        // [1, 4, 5].reduce((s, x) => s + x, 0) -> 10
+        const etiquetas = this.collection.reduce((etiquetas, producto) => {
+            // for (let nombre of [Math.floor(10 * producto.get("precio") / 1000)]) {
+            for (let nombre of producto.get("etiquetas")) {
+                if (nombre in etiquetas) {
+                    etiquetas[nombre] += 1;
+                } else {
+                    etiquetas[nombre] = 1;
+                }
+            }
+            return etiquetas;
+        }, {});
+
+        this.$el.html(this.template({
+            total: this.collection.length,
+            etiquetas: etiquetas
+        }));
     }
 });
